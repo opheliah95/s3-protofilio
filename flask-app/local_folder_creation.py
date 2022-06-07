253e86b7feaf963fd8e74ef9d7c5482da9249402
opheliah95 <opheliah95@gmail.com>
2022-06-08 00:52:55 +0100

    enabled folder creatopn function

--- flask-app/local_folder_creation.py
+++ flask-app/local_folder_creation.py
@@ -8,6 +8,8 @@ load_dotenv()
 # cast folder to type list
 folders: list = json.loads(os.environ.get("FOLDERS", "default"))
 dir_name = os.environ.get("DIR_NAME", "img")
+
+# some code to debug whether folder worked
 print(folders)
 print("type of folder: ", type(folders))
 for i in folders:
@@ -21,10 +23,10 @@ if not os.path.exists(dir_name):
 dir_path = os.path.join(dir_name)
 print(dir_path)
 
-# for f in folders:
-#     full_path = os.path.join(dir_path, f)
-#     if os.path.exists(full_path):
-#         print(f"{full_path} already exists")
-#     else:
-#         os.makedirs(full_path)
-#         print(f"{full_path} has been created")
+for f in folders:
+    full_path = os.path.join(dir_path, f)
+    if os.path.exists(full_path):
+        print(f"{full_path} already exists")
+    else:
+        os.makedirs(full_path)
+        print(f"{full_path} has been created")
