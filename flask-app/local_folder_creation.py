93bcd79762e3726c5e2b9bd6fa4a797910b82caa
opheliah95 <opheliah95@gmail.com>
2022-06-08 00:52:13 +0100

    parse env folder as list

--- flask-app/local_folder_creation.py
+++ flask-app/local_folder_creation.py
@@ -2,11 +2,16 @@ import os
 
 # load environment variable
 from dotenv import load_dotenv
+import json
 load_dotenv()
 
-folders = os.environ.get("FOLDERS", "default")
+# cast folder to type list
+folders: list = json.loads(os.environ.get("FOLDERS", "default"))
 dir_name = os.environ.get("DIR_NAME", "img")
 print(folders)
+print("type of folder: ", type(folders))
+for i in folders:
+    print(i)
 
 # check if dir already exist at root level
 if not os.path.exists(dir_name):
@@ -16,10 +21,10 @@ if not os.path.exists(dir_name):
 dir_path = os.path.join(dir_name)
 print(dir_path)
 
-for f in folders:
-    full_path = os.path.join(dir_path, f)
-    if os.path.exists(full_path):
-        print(f"{full_path} already exists")
-    else:
-        os.makedirs(full_path)
-        print(f"{full_path} has been created")
+# for f in folders:
+#     full_path = os.path.join(dir_path, f)
+#     if os.path.exists(full_path):
+#         print(f"{full_path} already exists")
+#     else:
+#         os.makedirs(full_path)
+#         print(f"{full_path} has been created")
