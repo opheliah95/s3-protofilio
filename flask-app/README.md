38a7c6554bd698aeb4bcf777e9eff4c0857a73a0
opheliah95 <opheliah95@gmail.com>
2022-06-08 00:12:28 +0100

    feat: boto to query data

---
+++ flask-app/README.md
@@ -0,0 +1,6 @@
+# Protofilo on S3
+
+This project is consolidated place that I will be able to show all my protofilo project, basing on catagories
+
+I will create some python scripts to setup the bucket and upload screenshots/gifs as well as any weblinks into the bucket
+
--- flask-app/s3.py
+++ flask-app/s3.py
@@ -1,26 +1,35 @@
 import boto3
 import os
 from botocore.client import ClientError
-
+from botocore.errorfactory import BucketAlreadyExists
 # load environment variable
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # our very secret buckets
 bucket = os.environ.get("BUCKET_NAME", "default")
+region = os.environ.get("REGION", "eu-west-2")
 print(f"bucket name is {bucket}")
 
 
-
 # use s3 bucket as resource
-s3 = boto3.resource('s3')
+s3 = boto3.resource("s3")
 
 try:
-    s3.meta.client.head_bucket(Bucket=bucket.name)
+    result = s3.meta.client.head_bucket(Bucket=bucket)
+    print(result)
 except ClientError:
     # The bucket does not exist or you have no access.
+    print(f"bucket {bucket} not found")
+    # create a bucket if not exist
+    s3.create_bucket(
+        Bucket=bucket, CreateBucketConfiguration={"LocationConstraint": region}
+    )
+except BucketAlreadyExists:
 
- # list all our buckets
+
+# list all our buckets
 def list_all_buckets(s3):
     for bucket in s3.buckets.all():
         print(bucket.name)
